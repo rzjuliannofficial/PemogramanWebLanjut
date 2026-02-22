9b094406f7336558eb07a994daebce57aa4d66c2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-02-22 16:55:54 +0700

    add routes at praktikum1(web.php) n make laporan for yang ditambahkan

--- jobsheet02/routes/web.php
+++ jobsheet02/routes/web.php
@@ -6,9 +6,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 1
 Route::get('/hello', function () {
     return 'Hello World V2';
 });
 Route::get('/world', function () {
     return 'World V2';
+});
+
+// 2
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
 });
