b4ce4a205f8b11bac5f782ea7c6e1e5cccb6da39
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-02-22 16:42:52 +0700

    add  route for praktikum1 n img_laporan in readme

--- jobsheet02/routes/web.php
+++ jobsheet02/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World V2';
+});
+Route::get('/world', function () {
+    return 'World V2';
+});
