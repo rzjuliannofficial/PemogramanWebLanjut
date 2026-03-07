9ccb29bb03d777ecc5af03dc2827282bdbb33191
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-03-07 16:05:24 +0700

    add user creation functionality in UserController and update fillable fields in UserModel

--- PWL-POS/app/Http/Controllers/UserController.php
+++ PWL-POS/app/Http/Controllers/UserController.php
@@ -10,11 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
